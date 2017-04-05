90d27c9fe7cab7f7cb7d0f35993fc9d7eb2c0b73
CF12 <brianx0011@gmail.com>
2017-04-05 07:08:43 -0400

    Work on sign in auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-// function easeLoadIn() {
-//
-// }
+const $ = require('jquery')
+const fb = require('firebase')
+const gAuthProvider = new fb.auth.GoogleAuthProvider()
+
+const fbConfig = {
+  apiKey: 'AIzaSyBgiv2CAsvosW3RFsS8SYv9JhVeYJLWRYo',
+  authDomain: 'lcc-wifi.firebaseapp.com',
+  databaseURL: 'https://lcc-wifi.firebaseio.com',
+  projectId: 'lcc-wifi',
+  storageBucket: 'lcc-wifi.appspot.com',
+  messagingSenderId: '1038104580431'
+}
+
+fb.initializeApp(fbConfig)
+
+fb.auth().onAuthStateChanged((user) => {
+  console.log(user)
+})
+
+function onSignIn (googleUser) {
+  const credential = gAuthProvider.credential(googleUser.idToken)
+  fb.auth().signInWithCredential(credential).catch((err) => {
+    alert('Login Error', err.message, [
+      {text: 'OK'}
+    ])
+  })
+}
 
 window.onload = () => {
   let loader = $('.wrapper-loader')
   setTimeout(() => {
-    loader.fadeOut({duation: 200})
+    loader.fadeOut({duration: 200})
   }, 1800)
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const {app, BrowserWindow} = require('electron')
 
+require('electron-debug')({showDevTools: true})
+
 app.on('ready', () => {
   let win = new BrowserWindow({width: 1200, height: 900})
 
-  win.loadURL(`file://${__dirname}/index.html`)
+  // win.loadURL(`file://${__dirname}/index.html`)
+  win.loadURL(`http://www.google.com/`)
+  console.log(win.setTitle)
 })
