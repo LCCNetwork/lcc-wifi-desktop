d620b749423cd3c4a3a6fd580e4c4cf2954b85e2
CF12 <brianx0011@gmail.com>
2017-04-10 23:21:41 -0400

    Major work on application look and style, and implemented googel OAuth2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const $ = require('jquery')
 const fb = require('firebase')
-require('google-oauth2')
+const remote = require('electron').remote
+const remoteMain = require('electron').remote.require('./main')
+const {app, dialog, BrowserWindow} = require('electron').remote
 const gAuthProvider = new fb.auth.GoogleAuthProvider()
+const imagesLoaded = require('imagesloaded')
+const ProgressBar = require('progressbar.js')
+
+let filename = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)
 
 const fbConfig = {
   apiKey: 'AIzaSyBgiv2CAsvosW3RFsS8SYv9JhVeYJLWRYo',
@@ -14,39 +20,47 @@ const fbConfig = {
 
 fb.initializeApp(fbConfig)
 
-fb.auth().onAuthStateChanged((user) => {
-  console.log(user)
-})
-
-// Google OAuth Handlers
-function onSuccess (user) {
-    const credential = gAuthProvider.credential(user.idToken)
-  fb.auth().signInWithCredential(credential).catch((err) => {
-    alert('Login Error', err.message, [
-      {text: 'OK'}
-    ])
-  })
-}
-
-function onFailure (err) {
-    console.error(err)
-}
-
-function renderButton () {
-    gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
+function openOauthWin () {
+  remoteMain.openOauth()
+  .then(userData => {
+    const credential = gAuthProvider.credential(userData.id_token)
+    fb.auth().signInWithCredential(credential)
+    .then((info) => {
+      remote.getCurrentWindow().loadURL(`file://${__dirname}/app.html`)
     })
+    .catch((err) => {
+      console.log(err)
+      dialog.showErrorBox('Authentication Error', 'Firebase Error:\n' + err.message)
+    })
+  })
+  .catch(() => {})
+}
+
+function loadApp () {
+  let dataDisplay = new ProgressBar.Circle('.app-data-display', {
+    strokeWidth: 8,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#72CADA',
+    trailColor: '#FFF',
+    trailWidth: 8
+  })
+
+  dataDisplay.animate(0.55)
+}
+
+function loadIndex () {
+  let loader = $('.wrapper-loader')
+  loader.fadeOut({duration: 500})
 }
 
 window.onload = () => {
-  let loader = $('.wrapper-loader')
-  setTimeout(() => {
-    loader.fadeOut({duration: 200})
-  }, 1800)
+  console.log(filename)
+  if (filename === 'index.html') {
+    setTimeout(() => {
+      imagesLoaded('body', loadIndex())
+    }, 1200)
+  } else if (filename === 'app.html') {
+    loadApp()
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
 const {app, BrowserWindow} = require('electron')
-
-require('electron-debug')({showDevTools: true})
+const electronGoogleOauth = require('electron-google-oauth')
+const authConfig = require('../oauth2-config')
 
 app.on('ready', () => {
-  let win = new BrowserWindow({width: 1200, height: 900})
+  let win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    resizable: false
+  })
 
-  // win.loadURL(`file://${__dirname}/index.html`)
-  win.loadURL(`http://www.google.com/`)
-  console.log(win.setTitle)
+  win.loadURL(`file://${__dirname}/index.html`)
 })
+
+exports.openOauth = () => {
+  const browserWindowParams = {
+    'use-content-size': true,
+    center: true,
+    resizable: false,
+    'always-on-top': true,
+    'standard-window': true,
+    'auto-hide-menu-bar': true,
+    'node-integration': false
+  }
+  const googleOauth = electronGoogleOauth(browserWindowParams)
+
+  return googleOauth.getAccessToken(
+    ['profile', 'email'],
+    authConfig.client_id,
+    authConfig.client_secret
+  )
+  .catch(() => {})
+}
